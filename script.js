3702be3e3b3e067bfa3f6206774472a549c089ff
EA - Kayque Job Da Silva <kayque_job@educadventista.org>
2024-10-11 07:54:50 -0300

    javascript - 1

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document.querySelector("button")
+button.addEventListener("click", multiplicar)
+
+function multiplicar() {
+    const numero1 = Number(document.querySelector("#numero-1").value)
+    const numero2 = Number(document.querySelector("#numero-1").value)
+    const numero3 = Number(document.querySelector("#numero-1").value)
+
+    const resultado= document.querySelector(".resultado")
+    reswultad
+}
